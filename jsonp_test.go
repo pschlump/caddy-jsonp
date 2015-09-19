15ad874f5d7d06b7bc24427955ac3cc78ee9fd23
Philip Schlump <pschlump@gmail.com>
2015-09-19 09:57:55 -0600

    Passing test

--- jsonp_test.go
+++ jsonp_test.go
@@ -9,12 +9,15 @@ package jsonp
 //
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/mholt/caddy/middleware"
 )
+
+const db_test = false
 
 var TestCases = []struct {
 	paths        []string
@@ -28,12 +31,21 @@ var TestCases = []struct {
 		`{"ok":123}`,
 		`xyz({"ok":123});`,
 	},
+	{
+		[]string{"/abc", "/def"},
+		"/ghi?callback=xyz",
+		`{"ok":123}`,
+		`{"ok":123}`,
+	},
 }
 
 func TestIPFilter(t *testing.T) {
 	for _, tc := range TestCases {
+
 		aaa := JsonPHandlerType{
 			Next: middleware.HandlerFunc(func(w http.ResponseWriter, r *http.Request) (int, error) {
+				// w.Write([]byte(`{"ok":123}`))
+				w.Write([]byte(tc.input))
 				return http.StatusOK, nil
 			}),
 			Paths: tc.paths,
@@ -43,19 +55,25 @@ func TestIPFilter(t *testing.T) {
 		if err != nil {
 			t.Fatalf("Could not create HTTP request: %v", err)
 		}
+		req.RequestURI = tc.calls
 
 		rec := httptest.NewRecorder()
 
+		// Make the call to the server
 		status, err := aaa.ServeHTTP(rec, req)
 		if err != nil {
-			t.Fatalf("Responded with error: %v, TestCase: %v\n", err, tc)
+			t.Fatalf("Responded with error: %v, TestCase: %+v\n", err, tc)
 		}
 		if status != 200 {
-			t.Fatalf("Responded with invalid status: %v, TestCase: %v\n", err, tc)
+			t.Fatalf("Responded with invalid status: %v, TestCase: %+v\n", err, tc)
 		}
 
-		if rec.Body.String() != tc.expectedBody {
-			t.Fatalf("Expected Body: '%s', Got: '%s' TestCase: %v\n", tc.expectedBody, rec.Body.String(), tc)
+		resultBody := rec.Body.String()
+		if db_test {
+			fmt.Printf("body >%s<-\n", resultBody)
+		}
+		if resultBody != tc.expectedBody {
+			t.Fatalf("Expected Body: '%s', Got: '%s' TestCase: %+v\n", tc.expectedBody, rec.Body.String(), tc)
 		}
 	}
 }
