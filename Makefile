b69cb48dca153133252011531009e7427918df23
Philip Schlump <pschlump@gmail.com>
2015-11-05 11:51:14 -0700

    Fixed for version 0.8x of caddy

---
+++ Makefile
@@ -0,0 +1,8 @@
+
+all:
+	go build
+
+
+t1:
+	 ../../caddyserver/caddydev/caddydev --after gzip jsonp
+
--- jsonp.go
+++ jsonp.go
@@ -14,13 +14,17 @@
 package jsonp
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"net/url"
 
-	"github.com/mholt/caddy/config/setup"
-	"github.com/mholt/caddy/middleware"
+	"github.com/mholt/caddy/caddy/setup" // Old: version 0.7.6 "github.com/mholt/caddy/config/setup"
+	"github.com/mholt/caddy/middleware"  //  Old: version 0.7.6 "github.com/mholt/caddy/middleware"
 	"github.com/pschlump/caddy-jsonp/bufferhtml"
 )
+
+const db1 = false
 
 // Jsonp configures a new Jsonp middleware instance.
 func Setup(c *setup.Controller) (middleware.Middleware, error) {
@@ -29,7 +33,9 @@ func Setup(c *setup.Controller) (middleware.Middleware, error) {
 		return nil, err
 	}
 
-	// fmt.Printf("Setup called, paths=%s\n", paths)
+	if db1 {
+		fmt.Printf("Setup called, paths=%s\n", paths)
+	}
 
 	return func(next middleware.Handler) middleware.Handler {
 		return JsonPHandlerType{
@@ -42,7 +48,9 @@ func Setup(c *setup.Controller) (middleware.Middleware, error) {
 func jsonpParse(c *setup.Controller) ([]string, error) {
 	var paths []string
 
-	// fmt.Printf("jsonpParse called\n")
+	if db1 {
+		fmt.Printf("jsonpParse called\n")
+	}
 
 	for c.Next() {
 		if !c.NextArg() {
@@ -61,14 +69,34 @@ type JsonPHandlerType struct {
 
 func (jph JsonPHandlerType) ServeHTTP(www http.ResponseWriter, req *http.Request) (int, error) {
 
-	// fmt.Printf("JsonPHandlerType.ServeHTTP called\n")
+	if db1 {
+		fmt.Printf("JsonPHandlerType.ServeHTTP called\n")
+	}
 
 	for _, prefix := range jph.Paths {
+		if db1 {
+			fmt.Printf("Path Matches\n")
+		}
 		if middleware.Path(req.URL.Path).Matches(prefix) {
+			if db1 {
+				fmt.Printf("A\n")
+			}
 			ResponseBodyRecorder := bufferhtml.NewBufferHTML()
+			if db1 {
+				fmt.Printf("B\n")
+			}
 			status, err := jph.Next.ServeHTTP(ResponseBodyRecorder, req)
+			if db1 {
+				fmt.Printf("C status=%d err=%s\n", status, err)
+			}
 			if status == 200 || status == 0 {
+				if db1 {
+					fmt.Printf("D\n")
+				}
 				// if there is a "callback" argument then use that to format the JSONp response.
+				if db1 {
+					fmt.Printf("Inside, status=%d\n", status)
+				}
 				Prefix := ""
 				Postfix := ""
 				u, err := url.ParseRequestURI(req.RequestURI)
@@ -83,13 +111,20 @@ func (jph JsonPHandlerType) ServeHTTP(www http.ResponseWriter, req *http.Request
 				}
 				callback := m.Get("callback")
 				if callback != "" {
+					if db1 {
+						fmt.Printf("Callback = -[%s]-\n", callback)
+					}
 					www.Header().Set("Content-Type", "application/javascript")
 					Prefix = callback + "("
 					Postfix = ");"
 				}
 				ResponseBodyRecorder.FlushAtEnd(www, Prefix, Postfix)
 			} else {
-				ResponseBodyRecorder.FlushAtEnd(www, "", "")
+				if db1 {
+					fmt.Printf("E - error occured\n")
+				}
+				// ResponseBodyRecorder.FlushAtEnd(www, "", "")
+				log.Printf("Error (%s) status %d - from JSONP directive\n", err, status)
 			}
 			return status, err
 		}
